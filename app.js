4161b4dad80e489bab41da1a770b09d8c472da2e
mustapha chatbi <chatbimdev@gmail.com>
2021-10-26 21:02:29 +0100

    initial app layout

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 (function () {
     document.addEventListener("DOMContentLoaded", function () {
         let mainContainer = document.querySelector("#initial-main-container")
-        let secondaryContainer = document.querySelector("#secondary-main-container")
+        let secondaryContainer = document.querySelector("#initial-secondary-container")
     })
 })();
