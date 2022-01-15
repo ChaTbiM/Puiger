8e9880a1809579ad6930c2d35f83f983bd574631
mustapha chatbi <chatbimdev@gmail.com>
2022-01-15 21:15:53 +0100

    added types to app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,56 +1,58 @@
+enum VISIBILITY {
+    SHOW = "show",
+    HIDE = "hide"
+}
+
 (function () {
-    const SHOW = "show";
-    const HIDE = "hide";
-
     document.addEventListener("DOMContentLoaded", function () {
-        let mainContainer = document.querySelector("#initial-main-container")
-        let popupContainer = document.querySelector("#popup-container")
-        let closePopup = document.querySelector("#popup-close")
-        let overlay = document.querySelector(".overlay");
+        let mainContainer: HTMLElement = document.querySelector("#initial-main-container")
+        let popupContainer: HTMLElement = document.querySelector("#popup-container")
+        let closePopup: HTMLElement = document.querySelector("#popup-close")
+        let overlay: HTMLElement = document.querySelector(".overlay");
 
-        const toggleOverlay = (mode = HIDE) => {
-            if (mode === SHOW) {
-                overlay.classList.add("show")
-                overlay.classList.remove("hide")
-            } else if (mode === HIDE) {
-                overlay.classList.remove("show")
-                overlay.classList.add("hide")
+        const toggleOverlay = (mode = VISIBILITY.HIDE) => {
+            if (mode === VISIBILITY.SHOW) {
+                overlay.classList.add(VISIBILITY.SHOW)
+                overlay.classList.remove(VISIBILITY.HIDE)
+            } else if (mode === VISIBILITY.HIDE) {
+                overlay.classList.remove(VISIBILITY.SHOW)
+                overlay.classList.add(VISIBILITY.HIDE)
             }
         }
 
-        const togglePopup = (mode = HIDE) => {
-            if (mode === SHOW) {
-                popupContainer.classList.add("show")
-                popupContainer.classList.remove("hide")
-            } else if (mode === HIDE) {
-                popupContainer.classList.remove("show")
-                popupContainer.classList.add("hide")
+        const togglePopup = (mode = VISIBILITY.HIDE) => {
+            if (mode === VISIBILITY.SHOW) {
+                popupContainer.classList.add(VISIBILITY.SHOW)
+                popupContainer.classList.remove(VISIBILITY.HIDE)
+            } else if (mode === VISIBILITY.HIDE) {
+                popupContainer.classList.remove(VISIBILITY.SHOW)
+                popupContainer.classList.add(VISIBILITY.HIDE)
             }
         }
 
         closePopup.addEventListener("click", () => {
-            togglePopup(HIDE);
-            toggleOverlay(HIDE);
+            togglePopup(VISIBILITY.HIDE);
+            toggleOverlay(VISIBILITY.HIDE);
         })
 
         mainContainer.addEventListener("click", () => {
-            togglePopup(SHOW)
-            toggleOverlay(SHOW);
+            togglePopup(VISIBILITY.SHOW)
+            toggleOverlay(VISIBILITY.SHOW);
         })
 
         document.addEventListener("click", (event) => {
             event.stopImmediatePropagation()
-            let popupContainerNestedChildren = Array.prototype.slice.call(popupContainer.children);
+            let popupContainerNestedChildren: Array<HTMLElement> = Array.prototype.slice.call(popupContainer.children);
 
             for (let i = 0; i < 3; i++) {
                 popupContainerNestedChildren = [...popupContainerNestedChildren,
-                ...popupContainerNestedChildren.map((directChild) => Array.prototype.slice.call(directChild.children)).flat()
+                ...popupContainerNestedChildren.map((directChild: HTMLElement) => Array.prototype.slice.call(directChild.children)).flat()
                 ];
             }
 
-            if (event.target !== popupContainer && event.target !== mainContainer && !popupContainerNestedChildren.includes(event.target)) {
-                togglePopup(HIDE);
-                toggleOverlay(HIDE);
+            if (event.target !== popupContainer && event.target !== mainContainer && !popupContainerNestedChildren.includes(event.target as HTMLElement)) {
+                togglePopup(VISIBILITY.HIDE);
+                toggleOverlay(VISIBILITY.HIDE);
             }
         })
 
